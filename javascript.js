1d90765ab157abea634058e83962e01f62397025
YOGIPATEL07 <122917701+YOGIPATEL07@users.noreply.github.com>
2023-04-18 11:52:19 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    //calling the html
+      $(function(){ 
+          $("#head1").load("meta.html");
+          $("#header").load("header.html");  
+          $(".footer").load("footer.html");  
+        }); 
+    });  
